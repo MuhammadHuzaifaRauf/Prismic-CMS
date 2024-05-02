938319b170d9a47aee9d413352928da324354f40
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-02 14:31:10 +0500

    feat:Add HomePage Page Types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  return (
-    <main className="">
-      <h1 className="text-5xl font-body font-bold">Hello World!</h1>
-    </main>
-  );
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+
+export default async function Page() {
+  return <div className="text-red-900">Home Page</div>;
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title || "Flowrise fallback",
+    description:
+      page.data.meta_description || "Flowrise is the relaxing app for you",
+  };
 }
