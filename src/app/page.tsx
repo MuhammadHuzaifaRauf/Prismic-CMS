df36d4dfb35138a5a17b9aafadbfbb968053704e
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-02 12:55:56 +0500

    feat:Add fallback MetaData to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <main className="">
+      <h1 className="text-5xl font-body font-bold">Hello World!</h1>
+    </main>
+  );
 }
